a31249ec74d0f49f610283aae4f30222147fe587
SOku <alexandre.pellae@gmail.com>
2018-12-27 03:33:01 +0100

    Refactoring Modele

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=tp_blog_mvc_2;charset=utf8','root', '');
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-          . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-          . ' order by BIL_ID desc');
-// Affichage
+
+require 'model.php';
+
+$billets = getBillets();
+
 require 'view/view.php';
