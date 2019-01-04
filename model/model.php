88d8b433845946a956e22866205d620f60ef1bdb
SOku <alexandre.pellae@gmail.com>
2019-01-04 13:33:56 +0100

    Suppression code inutile Model.php

--- model/model.php
+++ model/model.php
@@ -9,15 +9,15 @@ abstract class Model {
 	protected function executerRequete($sql, $params = null) {
 		if ($params == null) 
 		{
-			$resultat = $this->getBdd()->query($sql); // execution directe
+			$resultat = $this->getDb()->query($sql); // execution directe
 		} else {
-			$resultat = $this->getBdd()->prepare($sql); // requête preparée
+			$resultat = $this->getDb()->prepare($sql); // requête preparée
 			$resultat->execute($params);
 		}
 		return $resultat;
 	}
 	
-	private function getBdd()
+	private function getDb()
 	{
 		if($this->bdd == null) 
 		{
@@ -26,24 +26,4 @@ abstract class Model {
 		}
     return $this->bdd;
   }
-	
-	// Renvoie la liste des commentaires associés à un billet
-	public function getCommentaires($idBillet)
-	{
-		$bdd = $this->getDb();
-		$commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
-		. ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
-		. ' where BIL_ID=?');
-		$commentaires->execute(array($idBillet));
-		
-		return $commentaires;
-	}
-	
-	// Connexion Base de données
-	private function getDb() 
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=tp_blog_mvc_2;charset=utf8','root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-		return $bdd;
-	}
-
 }
