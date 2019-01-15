bafe3bfae65c86fbd11e2ba513b86e1ca6586f6f
SOku <alexandre.pellae@gmail.com>
2019-01-15 23:52:59 +0100

    Ajout fonction getAccountInfo dans model admin.php

--- model/admin.php
+++ model/admin.php
@@ -5,10 +5,21 @@ require_once "model/model.php"
 Class Admin extends Model 
 
 {
+
+	// Recuperation infos compte
+	public function getAdminInfo($username)
+	{
+		$sql = "SELECT id, pass FROM members WHERE name = ?";
+		$admin = $this->executerRequete($sql, array($username));
+	}
+	
+	
 	// Liste commentaires signalés
-	public function getSignCom
+	public function getSignCom()
 	{
 		$sql = "SELECT * FROM `t_commentaire` WHERE COM_SIGNALER = 1";
+		$signCom = $this->executerRequete($sql);
+		return $signCom;
 	}
 
 }
--- view/viewConnexion.php
+++ view/viewConnexion.php
@@ -1,5 +1,5 @@
 <form class="formulaire" method="POST" action="index.php?action=connexion">
-	<p><label>Pseudonyme<input type="text"/></label></p>
-	<p><label>Mot de passe<input type="password"/></label></p>
+	<p><label>Pseudonyme<input type="text" name="username"/></label></p>
+	<p><label>Mot de passe<input type="password" name="password"/></label></p>
 	<input type="submit" value="Se connecter"/>
 </form>
