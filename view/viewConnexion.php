60739f963dd00e9e36f778fa07ee49615c8db0c2
SOku <alexandre.pellae@gmail.com>
2019-01-15 21:11:16 +0100

    Modif rapide style et balises formulaire connexion

--- view/viewConnexion.php
+++ view/viewConnexion.php
@@ -1,5 +1,5 @@
-<form method="POST" action="index.php?action=connexion">
-	<input type="text"/>
-	<input type="password"/>
+<form class="formulaire" method="POST" action="index.php?action=connexion">
+	<p><label>Pseudonyme<input type="text"/></label></p>
+	<p><label>Mot de passe<input type="password"/></label></p>
 	<input type="submit" value="Se connecter"/>
 </form>
